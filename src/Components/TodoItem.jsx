a760f36f377f7c02358b0a509bedbb804c05cb6c
Ramiro Alves <ramiroalvesprof@outlook.com>
2022-09-09 18:40:21 -0300

    Agregando comentarios a los componentes

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+// se recibe un elemento por cada iteracion que se realiza en TodoList y se crea un elemento de la lista
+// handleTodoClick ejecuta la funcion ToggleTodo que recibe el id del Item y actualiza el estado de completed
 export function TodoItem({todo, toggleTodo}){
     const {id, task, completed} = todo;
 
--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import {TodoItem} from './TodoItem'
 
+//todos es la lista de tareas que se reciben desde la App
+//aquí se itera por ella y se crea un TodoItem por cada elemento
+
+//toggleTodo es una funcion que actualiza el estado completed de las tareas (se envia como props a TodoItem)
+
 export function TodoList({todos, toggleTodo}) {
     return (
         <ul>
