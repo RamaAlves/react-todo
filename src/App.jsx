46c16800725197a61cef7b22b49ae5ad786c8f20
Ramiro Alves <ramiroalvesprof@outlook.com>
2022-09-09 18:31:16 -0300

    agregando componentes y funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, {useState, Fragment, useRef} from "react";
+import { TodoList } from "./Components/TodoList";
+import {v4 as uuidv4 } from "../node_modules/uuid/dist";
+
 
 export function App(){
+    const [todos, setTodos] = useState([
+        //estado por defecto
+        {id:1, task:"tarea 1", completed: false}
+    ])
+    
+    //agregar tareas realizadas a nueva lista en un p al final
+
+    //referencia para obtener el valor del input
+    const todoTaskRef = useRef();
+
+
+    //funcion para verificar el estado completed de cada tarea (se envia como prop hacia todo list y luego a todo item)
+    const toggleTodo= (id)=>{
+        //copia de lista de tareas
+        const newTodos = [...todos];
+        const todo = newTodos.find((todo)=>todo.id===id);
+        todo.completed= !todo.completed;
+        //actualizacion de lista de tareas
+        setTodos(newTodos)
+
+    }
+    //funcion agregar tareas a listado
+    const handleTodoAdd = ()=>{
+        const task= todoTaskRef.current.value;
+        //validando contenido del input
+        if (task=== '') return;
+        //agregando tarea a lista
+        setTodos((prevTodos)=>{
+            return [...prevTodos, {id:uuidv4(), task, completed: false}]
+        })
+        //reseteando input
+        todoTaskRef.current.value= null;
+    }
+    //funcion borrar tareas completas
+    const handleClearAll = ()=>{
+        //copia de lista de tareas solo de elementos sin completar
+        const newTodos = todos.filter((todo)=>!todo.completed);
+        setTodos(newTodos);
+    }
+
+
     return (
-    <h2>Hola Mundo!!!</h2>
+        <Fragment>
+                <TodoList todos={todos} toggleTodo={toggleTodo}/>
+                <input ref={todoTaskRef} type="text" placeholder="Nueva Tarea"></input>
+                <button onClick={handleTodoAdd}>➕</button>
+                <button onClick={handleClearAll}>🗑</button>
+                {/* se verifican las tareas completadas*/}
+                <p> Te quedan {todos.filter((todo)=>!todo.completed).length} tareas por terminar</p>
+        </Fragment>
     )
 }
--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -1,1 +1,17 @@
 import React from "react";
+
+export function TodoItem({todo, toggleTodo}){
+    const {id, task, completed} = todo;
+
+    const handleTodoClick=()=>{
+        //se recibio a traves de props, se ejecuta y devuelve los cambios
+        toggleTodo(id)
+    }
+
+    return (
+        <li>
+            <input type="checkbox" checked={completed} onChange={handleTodoClick}/>
+            {task}
+        </li>
+    )
+}
